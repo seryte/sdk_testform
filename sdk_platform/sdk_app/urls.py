ae0cbc83024b97d48776f7129a6df599bd9b015e
xukeze <xu.keze@intellif.com>
2018-11-08 17:27:27 +0800

    打通windows本地流程

--- sdk_platform/sdk_app/urls.py
+++ sdk_platform/sdk_app/urls.py
@@ -4,5 +4,7 @@ from sdk_app import views
 urlpatterns = [
     path('sdk_manage/', views.sdk_manage),
     path('sdk_test/', views.sdk_test),
+    path('linsdk_manage/', views.linsdk_manage),
+    path('upload/', views.upload)
 
 ]
--- sdk_platform/sdk_app/views.py
+++ sdk_platform/sdk_app/views.py
@@ -1,3 +1,4 @@
+import os, time
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
@@ -8,9 +9,30 @@ def sdk_manage(request):
     return render(request, "sdk_manage.html")
 
 
+def upload(request):
+    if request.method == 'POST':
+        file_obj = request.FILES.get('file')
+        f = open(os.path.join('upload', file_obj.name), 'wb')
+        print(file_obj, type(file_obj))
+        print(file_obj.name)
+        for chunk in file_obj.chunks():
+            f.write(chunk)
+        f.close()
+        return HttpResponse('OK')
+
+
 def sdk_test(request):
     if request.method == "POST":
         interface = request.POST.get("interface", "")
         algversion = request.POST.get("algversion", "")
         sdkversion = request.POST.get("sdkversion", "")
-    return HttpResponse(interface)
+        os.system("F:\\SDK\\Windows\\v2.1.2\\rc1\\run64.bat")
+        if interface == "人脸检测":
+            file = "F:\\SDK\\Windows\\v2.1.2\\rc1\\output\\detector_result\\log.txt"
+            with open(file, "r") as f:
+                return HttpResponse(f.read())
+    return HttpResponse(algversion)
+
+
+def linsdk_manage(request):
+    return render(request, "linsdk_manage.html")
