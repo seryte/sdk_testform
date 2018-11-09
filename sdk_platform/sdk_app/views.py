5c5777ddeff068aaf9bc4e630ebd00b5c88cdd4f
xukeze <xu.keze@intellif.com>
2018-11-09 14:43:23 +0800

    windows平台sdk功能基本实现

--- sdk_platform/sdk_app/views.py
+++ sdk_platform/sdk_app/views.py
@@ -1,4 +1,6 @@
-import os, time
+import os
+import re
+import subprocess
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
@@ -14,21 +16,85 @@ def upload(request):
         file_obj = request.FILES.get('file')
         f = open(os.path.join('upload', file_obj.name), 'wb')
         print(file_obj, type(file_obj))
-        print(file_obj.name)
         for chunk in file_obj.chunks():
             f.write(chunk)
         f.close()
         return HttpResponse('OK')
 
 
+# 工具-修改批处理文件
+def modifybat(file, old_str, new_str):
+    with open(file, "r", encoding="utf-8") as f1, \
+            open("%s.bak" % file, "w", encoding="utf-8") as f2:
+        new_str.replace(" ", "", 1)
+        for line in f1:
+            f2.write(re.sub(old_str, new_str, line))
+    os.remove(file)
+    os.rename("%s.bak" % file, file)
+
+
 def sdk_test(request):
     if request.method == "POST":
+        batdir = "F:\\SDK\\Windows\\v2.1.2\\rc1\\"
+        runbat = batdir + "run64.bat"
         interface = request.POST.get("interface", "")
         algversion = request.POST.get("algversion", "")
         sdkversion = request.POST.get("sdkversion", "")
-        os.system("F:\\SDK\\Windows\\v2.1.2\\rc1\\run64.bat")
+
         if interface == "人脸检测":
-            file = "F:\\SDK\\Windows\\v2.1.2\\rc1\\output\\detector_result\\log.txt"
+            modifybat(runbat, "rem call %dir_detector%", "call %dir_detector%")
+            os.system(runbat)
+            modifybat(runbat, "call %dir_detector%", "rem call %dir_detector%")
+            file = batdir + "output\\detector_result\\log.txt"
+            with open(file, "r") as f:
+                return HttpResponse(f.read())
+        elif interface == "特征提取":
+            modifybat(runbat, "rem call %dir_extractor%", "call %dir_extractor%")
+            os.system(runbat)
+            modifybat(runbat, "call %dir_extractor%", "rem call %dir_extractor%")
+            file = batdir + "output\\extractor_result\\log.txt"
+            with open(file, "r") as f:
+                return HttpResponse(f.read())
+        elif interface == "1比1比对":
+            modifybat(runbat, "rem call %dir_comparer%", "call %dir_comparer%")
+            os.system(runbat)
+            modifybat(runbat, "call %dir_comparer%", "rem call %dir_comparer%")
+            file = batdir + "output\\comparer_result\\log.txt"
+            with open(file, "r") as f:
+                return HttpResponse(f.read())
+        elif interface == "人脸搜索(1:N)":
+            modifybat(runbat, "rem call %dir_searcher%", "call %dir_searcher%")
+            os.system(runbat)
+            modifybat(runbat, "call %dir_searcher%", "rem call %dir_searcher%")
+            file = batdir + "output\\searcher_result\\log.txt"
+            with open(file, "r") as f:
+                return HttpResponse(f.read())
+        elif interface == "人脸属性":
+            modifybat(runbat, "rem call %dir_predictor%", "call %dir_predictor%")
+            os.system(runbat)
+            modifybat(runbat, "call %dir_predictor%", "rem call %dir_predictor%")
+            file = batdir + "output\\predictor_result\\log.txt"
+            with open(file, "r") as f:
+                return HttpResponse(f.read())
+        elif interface == "人脸抓拍":
+            modifybat(runbat, "rem call %dir_capturer%", "call %dir_capturer%")
+            os.system(runbat)
+            modifybat(runbat, "call %dir_capturer%", "rem call %dir_capturer%")
+            file = batdir + "output\\capturer_result\\log.txt"
+            with open(file, "r") as f:
+                return HttpResponse(f.read())
+        elif interface == "人脸跟踪":
+            modifybat(runbat, "rem call %dir_tracker%", "call %dir_tracker%")
+            os.system(runbat)
+            modifybat(runbat, "call %dir_tracker%", "rem call %dir_tracker%")
+            file = batdir + "output\\tracker_result\\log.txt"
+            with open(file, "r") as f:
+                return HttpResponse(f.read())
+        elif interface == "全部":
+            modifybat(runbat, "rem call %dir_", "call %dir_")
+            os.system(runbat)
+            modifybat(runbat, "call %dir_", "rem call %dir_")
+            file = os.system("")
             with open(file, "r") as f:
                 return HttpResponse(f.read())
     return HttpResponse(algversion)
