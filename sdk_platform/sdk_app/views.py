8366e33728048cb647e2419075c4f16dce0e150b
cydib <382643558@qq.com>
2018-11-17 12:04:41 +0800

    修改bat脚本，cpp设置了相对路径

--- sdk_platform/sdk_app/views.py
+++ sdk_platform/sdk_app/views.py
@@ -34,65 +34,65 @@ def modifybat(file, old_str, new_str):
 
 def sdk_test(request):
     if request.method == "POST":
-        batdir = "F:\\SDK\\Windows\\v2.1.2\\rc1\\"
+        batdir = "I:\\v2.1.2\\rc1\\"
         runbat = batdir + "run64.bat"
         interface = request.POST.get("interface", "")
         algversion = request.POST.get("algversion", "")
         sdkversion = request.POST.get("sdkversion", "")
 
         if interface == "人脸检测":
-            modifybat(runbat, "rem call %dir_detector%", "call %dir_detector%")
+            modifybat(runbat, "rem call DetectorTest.exe", "call DetectorTest.exe")
             os.system(runbat)
-            modifybat(runbat, "call %dir_detector%", "rem call %dir_detector%")
+            modifybat(runbat, "call DetectorTest.exe", "rem call DetectorTest.exe")
             file = batdir + "output\\detector_result\\log.txt"
             with open(file, "r") as f:
                 return HttpResponse(f.read())
         elif interface == "特征提取":
-            modifybat(runbat, "rem call %dir_extractor%", "call %dir_extractor%")
+            modifybat(runbat, "rem call ExtractorTest.exe", "call ExtractorTest.exe")
             os.system(runbat)
-            modifybat(runbat, "call %dir_extractor%", "rem call %dir_extractor%")
+            modifybat(runbat, "call ExtractorTest.exe", "rem call ExtractorTest.exe")
             file = batdir + "output\\extractor_result\\log.txt"
             with open(file, "r") as f:
                 return HttpResponse(f.read())
         elif interface == "1比1比对":
-            modifybat(runbat, "rem call %dir_comparer%", "call %dir_comparer%")
+            modifybat(runbat, "rem call ComparerTest.exe", "call ComparerTest.exe")
             os.system(runbat)
-            modifybat(runbat, "call %dir_comparer%", "rem call %dir_comparer%")
+            modifybat(runbat, "call ComparerTest.exe", "rem call ComparerTest.exe")
             file = batdir + "output\\comparer_result\\log.txt"
             with open(file, "r") as f:
                 return HttpResponse(f.read())
         elif interface == "人脸搜索(1:N)":
-            modifybat(runbat, "rem call %dir_searcher%", "call %dir_searcher%")
+            modifybat(runbat, "rem call SearcherTest.exe", "call SearcherTest.exe")
             os.system(runbat)
-            modifybat(runbat, "call %dir_searcher%", "rem call %dir_searcher%")
+            modifybat(runbat, "call SearcherTest.exe", "rem call SearcherTest.exe")
             file = batdir + "output\\searcher_result\\log.txt"
             with open(file, "r") as f:
                 return HttpResponse(f.read())
         elif interface == "人脸属性":
-            modifybat(runbat, "rem call %dir_predictor%", "call %dir_predictor%")
+            modifybat(runbat, "rem call PredictorTest.exe", "call PredictorTest.exe")
             os.system(runbat)
-            modifybat(runbat, "call %dir_predictor%", "rem call %dir_predictor%")
+            modifybat(runbat, "call PredictorTest.exe", "rem call PredictorTest.exe")
             file = batdir + "output\\predictor_result\\log.txt"
             with open(file, "r") as f:
                 return HttpResponse(f.read())
         elif interface == "人脸抓拍":
-            modifybat(runbat, "rem call %dir_capturer%", "call %dir_capturer%")
+            modifybat(runbat, "rem call CapturerTest.exe", "call CapturerTest.exe")
             os.system(runbat)
-            modifybat(runbat, "call %dir_capturer%", "rem call %dir_capturer%")
+            modifybat(runbat, "call CapturerTest.exe", "rem call CapturerTest.exe")
             file = batdir + "output\\capturer_result\\log.txt"
             with open(file, "r") as f:
                 return HttpResponse(f.read())
         elif interface == "人脸跟踪":
-            modifybat(runbat, "rem call %dir_tracker%", "call %dir_tracker%")
+            modifybat(runbat, "rem call TrackerTest.exe", "call TrackerTest.exe")
             os.system(runbat)
-            modifybat(runbat, "call %dir_tracker%", "rem call %dir_tracker%")
+            modifybat(runbat, "call TrackerTest.exe", "rem call TrackerTest.exe")
             file = batdir + "output\\tracker_result\\log.txt"
             with open(file, "r") as f:
                 return HttpResponse(f.read())
         elif interface == "全部":
-            modifybat(runbat, "rem call %dir_", "call %dir_")
+            modifybat(runbat, "rem call", "call")
             os.system(runbat+">>"+batdir+"all_log.txt")
-            modifybat(runbat, "call %dir_", "rem call %dir_")
+            modifybat(runbat, "call", "rem call")
             file = batdir + "all_log.txt"
             with open(file, "r") as f:
                 return HttpResponse(f.read())
